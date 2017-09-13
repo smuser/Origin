0bdaddfaaf411eba899709db4b31e9521857808a
shaheen.mirza <shaheen.mirza@MLT290.igglobal.com>
2017-09-13 14:47:57 +0530

    first output change

--- Testing/src/Sorting.java
+++ Testing/src/Sorting.java
@@ -16,7 +16,7 @@ public class Sorting {
 		      System.out.println("Sorterd data: " +c);
 		    }
 		    int data1[] = { 41, -15, 12, 61, 11 };
-			 
+			System.out.println("Testing sorting checkin update"); 
 		    Arrays.sort(data1);
 		 
 		    for (int c1: data1) 
