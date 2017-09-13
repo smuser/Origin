a2cf3dc9072dbc652797544311651966f1067767
shaheen.mirza <shaheen.mirza@MLT290.igglobal.com>
2017-09-13 14:36:15 +0530

    sorting checkin

---
+++ Testing/src/Sorting.java
@@ -0,0 +1,28 @@
+//Using sort method
+import java.util.Arrays;
+
+public class Sorting {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		{
+		    int data[] = { 4, -5, 2, 6, 1 };
+		 
+		    Arrays.sort(data);
+		 
+		    for (int c: data) 
+		    {
+		      System.out.println("Sorterd data: " +c);
+		    }
+		    int data1[] = { 41, -15, 12, 61, 11 };
+			 
+		    Arrays.sort(data1);
+		 
+		    for (int c1: data1) 
+		    {
+		      System.out.println("Sorterd data: " +c1);
+		    }
+		 }
+    }
+}
